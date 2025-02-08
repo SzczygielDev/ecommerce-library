c4187fac545a5cfe734d294ca5ded7a91aaf071f
SzczygielDev <32822239+SzczygielDev@users.noreply.github.com>
2025-02-08 23:34:21 +0100

    Added support for sending queries through the Mediator abstraction.

--- src/main/kotlin/pl/szczygieldev/ecommercelibrary/command/MediatorFacade.kt
+++ src/main/kotlin/pl/szczygieldev/ecommercelibrary/command/MediatorFacade.kt
@@ -2,11 +2,16 @@ package pl.szczygieldev.ecommercelibrary.command
 
 import arrow.core.Either
 import com.trendyol.kediatr.Mediator
+import com.trendyol.kediatr.Query
 
 class MediatorFacade(val kediatr: Mediator) :
     pl.szczygieldev.ecommercelibrary.command.Mediator {
 
     override suspend fun <T : CommandError> send(command: Command<T>): Either<T, Unit> {
         return kediatr.send(command)
+    }
+
+    override suspend fun <TQuery : Query<TResponse>, TResponse> send(query: TQuery): TResponse {
+        return kediatr.send(query)
     }
 }
